48f803a2626eb2c3739103ab19e67b6ebc32406b
Loic Abena <loic.abena@abyster.com>
2024-06-06 11:56:13 +0200

    Added toggle checkbox

--- src/UI/PlanSelection.tsx
+++ src/UI/PlanSelection.tsx
@@ -1,5 +1,6 @@
 import { CardHeader, PlanCard } from "../Components"
 import { plan } from "../data"
+import { toggleCheckBoxStyle } from "../global"
 
 export const PlanSelection = () => {
     return (
@@ -8,7 +9,7 @@ export const PlanSelection = () => {
                 title="Select your plan"
                 description="You have the option of monthly or yearly billing."
             />
-            <div className="flex gap-5">
+            <div className="flex gap-5 mb-10">
                 {plan.map(({ alt, image, plan, price, trailDuration }, index: number) => (
                     <PlanCard
                         key={index}
@@ -19,6 +20,16 @@ export const PlanSelection = () => {
                         trailDuration={trailDuration}
                     />
                 ))}
+            </div>
+
+            <div className="p-5 text-marine-blue flex justify-center text-center bg-magnolia font-medium rounded-[5px]">
+                <p>Monthly</p>
+                <input
+                    className={`${toggleCheckBoxStyle} ml-8 mr-8`}
+                    type="checkbox"
+                    role="switch"
+                />
+                <p>Yearly</p>
             </div>
         </>
     )
